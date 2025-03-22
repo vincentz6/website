c6168cd07ae75152b7643485c4a194714d7782b8
vincentz6 <55777027+vincentz6@users.noreply.github.com>
2025-03-22 17:52:34 -0600

    adding theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,21 @@
-import { createTheme } from '@mantine/core';
+import { createTheme, MantineColorsTuple } from '@mantine/core';
+
+const bluegray: MantineColorsTuple = [
+  '#f1f4fe',
+  '#e4e6ed',
+  '#c8cad3',
+  '#a9adb9',
+  '#9094a3',
+  '#7f8496',
+  '#777c91',
+  '#656a7e',
+  '#595e72',
+  '#4a5167'
+];
 
 export const theme = createTheme({
-  /** Put your mantine theme override here */
+  
+  colors: {
+    bluegray
+  },
 });
