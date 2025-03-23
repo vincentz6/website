c5c66914f9214f362c1cbd4046b4015ac8511832
vincentz6 <55777027+vincentz6@users.noreply.github.com>
2025-03-23 14:40:31 -0600

    appshell skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import "@mantine/core/styles.css";
 
-import { MantineProvider } from "@mantine/core";
+import { AppShell, MantineProvider } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { Router } from "./Router";
 import { theme } from "./theme";
 
 export default function App() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <MantineProvider theme={theme}>
-      <Router />
+      <AppShell
+        layout="alt"
+        header={{ height: { base: 60, md: 70, lg: 80 } }}
+        navbar={{
+          width: { base: 200, md: 300, lg: 400 },
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>header</AppShell.Header>
+        <AppShell.Navbar>navbar</AppShell.Navbar>
+        <AppShell.Main>
+          <Router />
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
   );
 }
