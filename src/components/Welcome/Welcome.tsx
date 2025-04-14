90e76eb1d178b204d8e4020e27b20b9b0b6a06e1
vincentz6 <55777027+vincentz6@users.noreply.github.com>
2025-04-14 12:58:21 -0600

    responsive testing

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,26 +1,42 @@
-import { Group } from "@mantine/core";
+import { Flex, Center } from "@mantine/core";
 import { TypeAnimation } from "react-type-animation";
 import { Avatar } from "../Avatar/Avatar";
 import classes from "./Welcome.module.css";
+
 export function Welcome() {
   return (
-    <Group>
-      <Avatar />
+    <Flex
+      direction={{
+        base: "column",
+        xs: "column",
+        sm: "column",
+        md: "column",
+        lg: "row",
+        xl: "row",
+      }}
+      align="center"
+      justify="center"
+      gap="xl"
+      className={classes.welcome}
+    >
+      <Center>
+        <Avatar />
+      </Center>
       <TypeAnimation
         sequence={[
           "Hello! I'm Vincent,\n Aspiring Developer.",
-          5000,
-          "Hello! I'm Vincent,\n Gym Enthusiast.",
-          1500,
-          "Hello! I'm Vincent,\n Knowledge Seeker.",
-          1500,
-          "Hello! I'm Vincent,\n Gaming Enjoyer.",
-          1500,
+          6000,
+          "Hello! I'm Vincent,\n Description1.",
+          2000,
+          "Hello! I'm Vincent,\n Description2.",
+          2000,
+          "Hello! I'm Vincent,\n Description3.",
+          2000,
         ]}
         wrapper="span"
         repeat={Infinity}
         className={classes.title}
       />
-    </Group>
+    </Flex>
   );
 }
