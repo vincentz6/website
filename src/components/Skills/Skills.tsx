e85fe9e5851c913479bb847664d9372cfa90c129
vincentz6 <55777027+vincentz6@users.noreply.github.com>
2025-06-23 15:31:27 -0600

    done skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,66 +1,14 @@
 import { Box, SimpleGrid } from "@mantine/core";
 import { Heading } from "../Heading/Heading";
 import { SkillCard } from "../SkillCard/SkillCard";
-import {
-  IconDeviceImacCode,
-  IconLayoutDashboard,
-  IconChartDots,
-  IconServer,
-  IconDatabase,
-  IconRocket,
-} from "@tabler/icons-react";
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
+import { cards } from "../../skills";
 // import classes from "./Skills.module.css";
 
 export function Skills() {
   const containerRef = useRef(null);
   const inView = useInView(containerRef, { once: true });
-
-  const cards = [
-    {
-      title: "Frontend Development",
-      description:
-        "To test the design of the website, I'm currently writing random shit and you're wasting your time reading this. This is sentence 2 used for testing the length of the card description.",
-      icon: IconDeviceImacCode,
-      keywords: ["HTML", "CSS", "JavaScript", "React", "Typescript"],
-    },
-    {
-      title: "UI/UX Design",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      icon: IconLayoutDashboard,
-      keywords: ["Figma"],
-    },
-    {
-      title: "Data Analysis",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      icon: IconChartDots,
-      keywords: ["Python", "Pandas", "NumPy"],
-    },
-    {
-      title: "Backend Development",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      icon: IconServer,
-      keywords: ["Java", "Node.js", "Django", "RESTful APIs"],
-    },
-    {
-      title: "Database Management",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      icon: IconDatabase,
-      keywords: ["SQL", "PostgreSQL", "MySQL", "MongoDB"],
-    },
-    {
-      title: "Deployment",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      icon: IconRocket,
-      keywords: ["Docker", "Netlify", "GCP"],
-    },
-  ];
 
   return (
     <Box mih="100vh" py="xl" px="md" bg="gradient1">
---
+++ src/skills.ts
@@ -0,0 +1,53 @@
+import {
+  IconDeviceImacCode,
+  IconLayoutDashboard,
+  IconChartDots,
+  IconServer,
+  IconDatabase,
+  IconRocket,
+} from "@tabler/icons-react";
+
+export const cards = [
+  {
+    title: "Frontend Development",
+    description:
+      "I build websites that are visually appealing and prioritize user experience. My goal is to create intuitive, responsive web applications that users enjoy interacting with.",
+    icon: IconDeviceImacCode,
+    keywords: ["HTML", "CSS", "JavaScript", "React", "Typescript"],
+  },
+  {
+    title: "UI/UX Design",
+    description:
+      "I design user interfaces that balance functionality with aesthetics, focusing on creating engaging experiences through wireframes, prototypes, and the refinement of final designs.",
+    icon: IconLayoutDashboard,
+    keywords: ["Figma"],
+  },
+  {
+    title: "Data Analysis",
+    description:
+      "Using tools like Pandas, I've analyzed and transformed data into insights that drive decision-making or simply uncover trends and patterns.",
+    icon: IconChartDots,
+    keywords: ["Python", "Pandas", "NumPy"],
+  },
+  {
+    title: "Backend Development",
+    description:
+      "I have experience in building robust, scalable backend systems using various technlogies. I've created RESTful APIs and integrated databases to support frontend applications.",
+    icon: IconServer,
+    keywords: ["Java", "Node.js", "Django", "RESTful APIs"],
+  },
+  {
+    title: "Database Management",
+    description:
+      "I maintain databases to ensure smooth and efficient data storage and retrieval. Whether it's relational or NoSQL, I focus on structuring data effectively and optimizing queries.",
+    icon: IconDatabase,
+    keywords: ["SQL", "PostgreSQL", "MySQL", "MongoDB"],
+  },
+  {
+    title: "Deployment",
+    description:
+      "Used tools to deploy and manage applications, ensuring they run smoothly. I focus on reliability and scalability through containerization and cloud services.",
+    icon: IconRocket,
+    keywords: ["Docker", "Netlify", "GCP"],
+  },
+];
