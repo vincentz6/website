7d4127e1643215d044c69c3a793e1e59445c60a9
vincentz6 <55777027+vincentz6@users.noreply.github.com>
2025-07-02 16:53:26 -0600

    motion

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import { Card, Image, Title, Text, Button, Stack } from "@mantine/core";
+import { motion } from "framer-motion";
 import classes from "./ProjectCard.module.css";
 
 type ProjectCardProps = {
@@ -15,25 +16,37 @@ export function ProjectCard({
   link,
 }: ProjectCardProps) {
   return (
-    <Card radius="md" shadow="sm" className={classes.card}>
-      <Card.Section>
-        <Image src={image} alt="Project Image" h={300} />
-      </Card.Section>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 1,
+        ease: "easeInOut",
+      }}
+      viewport={{ once: true }}
+    >
+      <Card radius="md" shadow="sm" className={classes.card}>
+        <Card.Section>
+          <Image src={image} alt="Project Image" h={300} />
+        </Card.Section>
 
-      <Stack h="100%" justify="space-between" className={classes.content}>
-        <div>
-          <Title order={3}>{title}</Title>
-          <Text>{description}</Text>
-        </div>
-        <Button.Group className={classes.buttons} orientation="vertical">
-          <Button component="a" href={link} target="_blank" bg="gradient1">
-            VIEW PROJECT
-          </Button>
-          <Button component="a" href={link} target="_blank" bg="gradient2">
-            SOURCE CODE
-          </Button>
-        </Button.Group>
-      </Stack>
-    </Card>
+        <Stack h="100%" justify="space-between" className={classes.content}>
+          <div>
+            <Title order={3} my="sm">
+              {title}
+            </Title>
+            <Text>{description}</Text>
+          </div>
+          <Button.Group className={classes.buttons} orientation="vertical">
+            <Button component="a" href={link} target="_blank" bg="gradient1">
+              VIEW PROJECT
+            </Button>
+            <Button component="a" href={link} target="_blank" bg="gradient2">
+              SOURCE CODE
+            </Button>
+          </Button.Group>
+        </Stack>
+      </Card>
+    </motion.div>
   );
 }
